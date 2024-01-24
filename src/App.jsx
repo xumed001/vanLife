6185aeebecd2b4d07e0d2a0085eb5f0cbd5dbf39
Sumed Tamang <xumed001@gmail.com>
2024-01-24 09:29:40 -0600

    added Layout for nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../server/vansData'
 
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer"
+import Layout from "./components/Layout";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
@@ -13,14 +12,14 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
