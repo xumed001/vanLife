54ff34d6baa491b55dc74396545afb9b96ff0068
Sumed Tamang <xumed001@gmail.com>
2024-01-24 18:32:42 -0600

    fix: Home page button + minor css changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,11 @@ function Home() {
         <div className="home--content">
             <h1 className="home--title">You got the travel plans, we got the travel vans.</h1>
             <p className="home--text">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Button className={'button orange'} >
-              <Link to={"/vans"}>Find your van</Link>
-            </Button>
+            <Link to={"/vans"}>
+              <Button className={'button orange'} >
+                Find your van
+              </Button>
+            </Link>
         </div>
     </div>
   )
